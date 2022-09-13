cd7d88bf2696f3fcf297cab884a0c5169a67693b
rejoan121615 <wearecrx@gmail.com>
2022-09-13 16:11:22 +0600

    main section complete

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-  res.send('hello rejoan')
-  // next()
-})
+// setup view
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "./public")));
 
+//  routes
+app.use("/", (req, res, next) => {
+    res.render("pages/home");
+    // next()
+});
 
-
-app.listen(3000, console.log('app started on http://localhost:3000 port'))
+// start application
+app.listen(3000, console.log("app started on http://localhost:3000 port"));
